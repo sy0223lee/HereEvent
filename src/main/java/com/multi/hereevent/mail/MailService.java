ee7a7dee9f3d0d15fb08eb0c32ed86e9dfd5b9ef
sy0223lee <sy0223lee@gmail.com>
2024-07-05 13:51:52 +0900

    feat: 대기 성공 메일 메시지 생성

--- src/main/java/com/multi/hereevent/mail/MailService.java
+++ src/main/java/com/multi/hereevent/mail/MailService.java
@@ -6,6 +6,7 @@ import com.multi.hereevent.dto.WaitDTO;
 import com.multi.hereevent.event.EventDAO;
 import com.multi.hereevent.event.EventService;
 import com.multi.hereevent.member.MemberService;
+import com.multi.hereevent.wait.WaitService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
@@ -21,13 +22,17 @@ public class MailService {
     private final JavaMailSender mailSender; // html 을 보내기 위한 mail sender
     public final MemberService memberService;
     public final EventService eventService;
+    public final WaitService waitService;
 
     /* 대기 3팀 남았을 경우 이메일 보내기 */
 
     /* 대기 등록 성공 시 이메일 보내기 */
     public void sendWaitSuccessEmail(WaitDTO wait) throws MessagingException {
-        // 대기한 이벤트 정보 가져오기
-        EventDTO event = eventService.getEventDetails(wait.getEvent_no());
+        // 매개변수로 받아온 wait 에는 wait_no가 포함되어 있지 않으므로
+        // 삽입된 대기의 wait_no 를 따로 가져오기
+        WaitDTO curWait = waitService.getWaitInfo(wait.getWait_tel());
+        // wait_no 로 이벤트 정보를 포함한 waitDTO 가져오기
+        WaitDTO eventWait = waitService.eventDetail(curWait.getWait_no());
 
         // 이메일 전송
         MimeMessage message = mailSender.createMimeMessage();
@@ -35,13 +40,35 @@ public class MailService {
 
         messageHelper.setFrom("sy0223lee@gmail.com");
         messageHelper.setTo(wait.getEmail());
-        messageHelper.setSubject("[HereEvent] '" + event.getName() + "' 대기 등록 안내");
-        messageHelper.setText(waitSuccessHTML(event), true);
+        messageHelper.setSubject("[HereEvent] '" + eventWait.getName() + "' 대기 등록 안내");
+        messageHelper.setText(waitHtml(eventWait, curWait.getWait_no(), curWait.getEvent_no()), true);
         mailSender.send(message);
     }
-    private String waitSuccessHTML(EventDTO event){
+    private String waitHtml(WaitDTO wait, int wait_no, int event_no){
+        // 메일로 보낼 정보 조회
+        int position = waitService.getWaitingPosition(event_no, wait_no); // 현재 내 순서
+        int waitingCount = waitService.getWaitingCount(event_no); // 현재 총 대기 인원
+        String waitTime = waitService.getEntranceWaitTime(event_no, wait_no); // 예상 대기 시간
 
-        return "";
+        // 메일로 보낼 메시지 생성
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html><body>");
+        sb.append("<meta http-equiv='Content-Type' content='text/html; charset=euc-kr'>");
+        sb.append("<h2>대기 중인 이벤트 안내</h2>")
+            .append("<div>Date()에 작성된 내용이며 실시간 현황 확인은 <a href=\"http://localhost:9090/hereevent/main\">HereEvent</a>에서 해주시길 바랍니다.</div>")
+            .append("    <img style='width:100px; height:100px; margin:10px 10px 10px 0;' src='http://localhost:9090/hereevent/download/event/wait.getImg_path()'/>")
+            .append("    <p>이벤트 제목 <strong>wait.getName()</strong></p>")
+            .append("    <p>이벤트 위치 <strong>wait.getAddr()</strong></p>")
+            .append("    <p>대기 번호 <strong><span>wait.getWait_no()</span></strong></p>")
+            .append("    <strong><p>waitTime</p></strong>")
+            .append("    <p>" )
+            .append("        <span style='display: inline;'>현재 내 순서 <strong>position번</strong></span>" )
+            .append("        <span style='display: inline; margin-left: 10px;'> 현재 대기 인원 <strong>waitingCount팀</strong></span>")
+            .append("    </p>")
+            .append("    <button onclick='location.href=\"http://localhost:9090/hereevent/main\"'>HereEvent 대기 현황 확인 바로가기</button>");
+
+
+        return sb.toString();
     }
 
     /* 추천 이메일 보내기 */
@@ -72,7 +99,7 @@ public class MailService {
                 "<td>이미지</td><td>이벤트명</td><td>기간</td><td>위치</td>" +
                 "</tr></thead><tbody>");
         for (EventDTO event : eventList) {
-            sb.append("<tr><td'><img style='width:100px; height:100px; margin:10px 10px 10px 0;' src='http://localhost:9090/hereevent/download/event/")
+            sb.append("<tr><td><img style='width:100px; height:100px; margin:10px 10px 10px 0;' src='http://localhost:9090/hereevent/download/event/")
                     .append(event.getImg_path()).append("'></td><td><a style='margin-right:10px;' href='http://localhost:9090/hereevent/event/")
                     .append(event.getEvent_no()).append("'>")
                     .append(event.getName()).append("</a></td><td><p style='margin-right:10px;'>")
