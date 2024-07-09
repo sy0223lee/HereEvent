cecd14f70f4358e212781cc4d29a3a91936da6b4
sunyoung9191 <parkson0408@gmail.com>
2024-07-09 16:53:54 +0900

    design: footer 디자인 변경 feat: 마이페이지 내에서 예약 취소, 입장 버튼 구현 minor: 오타 수정

--- src/main/java/com/multi/hereevent/reserve/ReserveService.java
+++ src/main/java/com/multi/hereevent/reserve/ReserveService.java
@@ -1,5 +1,11 @@
 package com.multi.hereevent.reserve;
 
+import com.multi.hereevent.dto.ReserveDTO;
+
+import java.util.Map;
+
 public interface ReserveService {
     String makeReservation(int event_no, int member_no, String reserve_date, String reserve_time);
+    int deleteReservation(Map<String, Object> params);
+    void updateReservation(Map<String, Object> params);
 }
