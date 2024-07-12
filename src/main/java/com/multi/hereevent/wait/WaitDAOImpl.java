a563d64bb9c9af1cb6c2e229e3b8b6202d578925
choibh17 <byu1117@naver.com>
2024-07-12 14:45:29 +0900

    design: wait css수정 fix: map import사라진것 수정

--- src/main/java/com/multi/hereevent/wait/WaitDAOImpl.java
+++ src/main/java/com/multi/hereevent/wait/WaitDAOImpl.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository
 @RequiredArgsConstructor
